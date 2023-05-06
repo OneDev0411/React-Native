eb91be85d8f4570ca575b1b96876dfc5a8e6f764
OneDev0411 <cryptoapple1109@gmail.com>
2023-05-06 12:50:21 +0200

    implemented google autocomplete

--- app/Sale.tsx
+++ app/Sale.tsx
@@ -1,31 +1,63 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, TextInput } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import { Button, Text, View } from '../components/Themed';
+import { useState } from 'react';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { MAPS_API_KEY } from '@env';
 
 export default function Sale() {
+	const [location, setLocation] = useState<{
+		name: string | undefined;
+		place_id: string | undefined;
+	}>({ name: undefined, place_id: undefined });
 	return (
-		<View style={styles.container}>
-			<Text style={styles.title}>Create a new Sale</Text>
-			<View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      
-		</View>
+		<>
+			{/* for some reason the GoogleMapsInput doesn't work when inside a <View /> */}
+
+			<GooglePlacesAutocomplete
+				styles={googleInputStyles}
+				placeholder="Example: Lotte Supermarket, New York"
+				onPress={(data, details = null) => {
+					setLocation({
+						name: data.structured_formatting.main_text,
+						place_id: data.place_id,
+					});
+				}}
+				query={{
+					key: MAPS_API_KEY,
+					language: 'en',
+				}}
+			/>
+			<Button
+				title={`Configure NFC card${location.name ? ` for ${location.name}` : ''}`}
+				disabled={!location.name && !location.place_id}
+				onPress={() => {
+					if (!location.place_id) return;
+					
+				}}
+			/>
+		</>
 	);
 }
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create({});
+
+const googleInputStyles = {
 	container: {
-		flex: 1,
-		alignItems: 'center',
-		justifyContent: 'center',
+		flex: 0,
 	},
-	title: {
-		fontSize: 20,
-		fontWeight: 'bold',
+	textInput: {
+		height: 38,
+		color: '#5d5d5d',
+		fontSize: 16,
+		textStyle: 'italic',
+		border: '1px solid red',
 	},
-	separator: {
-		marginVertical: 30,
-		height: 1,
-		width: '80%',
+	predefinedPlacesDescription: {
+		color: '#1faadb',
 	},
-});
+	textInputContainer: {
+		border: '1px solid red',
+	},
+};
