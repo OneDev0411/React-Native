dfdb7a630bd9084e92849181b0abefab28654caa
OneDev0411 <cryptoapple1109@gmail.com>
2023-05-06 14:40:00 +0200

    added NFC lib

--- functions/NFC.ts
+++ functions/NFC.ts
@@ -1,4 +1,18 @@
-export function writeGoogleLinkOnNFC(place_id: string) {
+import NfcManager, {NfcTech} from 'react-native-nfc-manager';
+
+export async function writeGoogleLinkOnNFC(place_id: string) {
     const reviewLink = `https://search.google.com/local/writereview?placeid=${place_id}`;
-    console.log(reviewLink);
+
+    try {
+		// register for the NFC tag with NDEF in it
+		await NfcManager.requestTechnology(NfcTech.Ndef);
+		// the resolved tag object will contain `ndefMessage` property
+		const tag = await NfcManager.getTag();
+		console.warn('Tag found', tag);
+	} catch (ex) {
+		console.warn('Oops!', ex);
+	} finally {
+		// stop the nfc scanning
+		NfcManager.cancelTechnologyRequest();
+	}
 }
