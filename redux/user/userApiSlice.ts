c056f2b52ed3a6cfa790a8305e587e0db6bbda13
OneDev0411 <cryptoapple1109@gmail.com>
2023-06-03 20:13:52 +0100

    added currency change

--- redux/user/userApiSlice.ts
+++ redux/user/userApiSlice.ts
@@ -1,50 +1,64 @@
-import { apiSlice } from "../api/apiSlice";
+import { apiSlice } from '../api/apiSlice';
 
 export const userSliceApi = apiSlice.injectEndpoints({
-  endpoints: (builder) => ({
-    getUser: builder.mutation({
-      query: () => ({
-        url: `/users/me`,
-        method: "GET",
-      }),
-    }),
-
-    submitApplication: builder.mutation({
-      query: (data) => ({
-        url: `/users/submitApplication`,
-        method: "POST",
-        body: data,
-      }),
-    }),
-    payoutMethod: builder.mutation({
-      query: (data) => ({
-        url: `/users/payoutMethod`,
-        method: "POST",
-        body: data,
-      }),
-    }),
-    deletePayoutMethod: builder.mutation({
-      query: (id) => ({
-        url: `/users/payoutMethod/${id}`,
-        method: "DELETE",
-      }),
-      invalidatesTags: ["Payouts"],
-    }),
-    getPayoutMethod: builder.query({
-      query: () => ({
-        url: `/users/payoutMethod`,
-        method: "GET",
-      }),
-      providesTags: ["Payouts"],
-    }),
-  }),
-  overrideExisting: true,
+	endpoints: (builder) => ({
+		getUser: builder.mutation({
+			query: () => ({
+				url: `/users/me`,
+				method: 'GET',
+			}),
+		}),
+		getCurrentUser: builder.query({
+			query: () => ({
+				url: `/users/me`,
+				method: 'GET',
+			}),
+		}),
+		submitApplication: builder.mutation({
+			query: (data) => ({
+				url: `/users/submitApplication`,
+				method: 'POST',
+				body: data,
+			}),
+		}),
+		payoutMethod: builder.mutation({
+			query: (data) => ({
+				url: `/users/payoutMethod`,
+				method: 'POST',
+				body: data,
+			}),
+		}),
+		deletePayoutMethod: builder.mutation({
+			query: (id) => ({
+				url: `/users/payoutMethod/${id}`,
+				method: 'DELETE',
+			}),
+			invalidatesTags: ['Payouts'],
+		}),
+		getPayoutMethod: builder.query({
+			query: () => ({
+				url: `/users/payoutMethod`,
+				method: 'GET',
+			}),
+			providesTags: ['Payouts'],
+		}),
+		updateUserCurrency: builder.mutation({
+			query: (data) => ({
+				url: `/users/currency`,
+				method: 'PUT',
+				body: data,
+			}),
+		}),
+	}),
+	overrideExisting: true,
 });
 
 export const {
-  useSubmitApplicationMutation,
-  useGetUserMutation,
-  usePayoutMethodMutation,
-  useGetPayoutMethodQuery,
-  useDeletePayoutMethodMutation,
+	useSubmitApplicationMutation,
+	useGetUserMutation,
+	useGetCurrentUserQuery,
+	usePayoutMethodMutation,
+	useGetPayoutMethodQuery,
+	useDeletePayoutMethodMutation,
+  useUpdateUserCurrencyMutation
 } = userSliceApi;
