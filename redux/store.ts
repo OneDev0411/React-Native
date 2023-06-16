e24d48c709e9a5350957d606df163ea30b737555
OneDev0411 <cryptoapple1109@gmail.com>
2023-06-16 18:56:08 +0100

    fix formatting

--- redux/store.ts
+++ redux/store.ts
@@ -1,45 +1,45 @@
-import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import {
-  persistReducer,
-  persistStore,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from "redux-persist";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { apiSlice } from "./api/apiSlice";
-import authReducer from "./auth/authSlice";
-import userReducer from "./user/userSlice";
-import saleReducer from "./sale/saleSlice";
+	persistReducer,
+	persistStore,
+	FLUSH,
+	REHYDRATE,
+	PAUSE,
+	PERSIST,
+	PURGE,
+	REGISTER,
+} from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { apiSlice } from './api/apiSlice';
+import authReducer from './auth/authSlice';
+import userReducer from './user/userSlice';
+import saleReducer from './sale/saleSlice';
 
 const persistConfig = {
-  key: "root",
-  storage: AsyncStorage,
-  whitelist: ["auth", "sale"],
-  blacklist: ["user"],
+	key: 'root',
+	storage: AsyncStorage,
+	whitelist: ['auth', 'sale'],
+	blacklist: ['user'],
 };
 
 const rootReduer = combineReducers({
-  [apiSlice.reducerPath]: apiSlice.reducer,
-  auth: authReducer,
-  user: userReducer,
-  sale: saleReducer,
+	[apiSlice.reducerPath]: apiSlice.reducer,
+	auth: authReducer,
+	user: userReducer,
+	sale: saleReducer,
 });
 
 const persistedReducer = persistReducer(persistConfig, rootReduer);
 
 export const store = configureStore({
-  reducer: persistedReducer,
+	reducer: persistedReducer,
 
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }).concat(apiSlice.middleware),
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+			},
+		}).concat(apiSlice.middleware),
 });
 
 export const persistor = persistStore(store);
