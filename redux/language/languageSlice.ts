ac28859e4e7c6c37b204c44d62b82e6ab8b4e793
OneDev0411 <cryptoapple1109@gmail.com>
2023-06-16 19:35:18 +0500

    re-added the expo localization

--- redux/language/languageSlice.ts
+++ redux/language/languageSlice.ts
@@ -1,10 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
-// import { getLocales } from "expo-localization";
+import { getLocales } from "expo-localization";
 
 const languageSlice = createSlice({
   name: "language",
-  // initialState: getLocales()[0].languageCode,
-  initialState: "en",
+  initialState: getLocales()[0].languageCode,
   reducers: {
     setLanguage: (state, action) => {
       return action.payload;
