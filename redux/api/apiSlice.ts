4c2211f5731a258985e435a75d9b9dd16f3e58f6
OneDev0411 <cryptoapple1109@gmail.com>
2023-06-16 19:51:23 +0100

    debug

--- redux/api/apiSlice.ts
+++ redux/api/apiSlice.ts
@@ -17,8 +17,9 @@ const baseQuery = fetchBaseQuery({
 
 const baseQueryWithReauth = async (args, api, extraOptions) => {
 	let result: any = '';
-
+	console.log('token expires at', api.getState()?.auth?.accessToken?.expires);
 	if (moment().isAfter(api.getState()?.auth?.accessToken?.expires)) {
+		console.log('token expired, refreshing...', api.getState()?.auth?.refreshToken?.token);
 		const refreshResult = await baseQuery(
 			{
 				url: '/auth/refresh-tokens',
@@ -35,9 +36,15 @@ const baseQueryWithReauth = async (args, api, extraOptions) => {
 			api.dispatch(setAccessToken(refreshResult?.data?.tokens?.access));
 			api.dispatch(setRefreshToken(refreshResult?.data?.tokens?.refresh));
 			result = await baseQuery(args, api, extraOptions);
+			console.log(
+				'refreshed token',
+				refreshResult?.data?.tokens?.access,
+				refreshResult?.data?.tokens?.refresh
+			);
 		} else if (refreshResult?.error) {
 			api.dispatch(logOut());
 			api.dispatch(apiSlice.util.resetApiState());
+			console.log('refresh token error', refreshResult?.error);
 		}
 	} else {
 		result = await baseQuery(args, api, extraOptions);
