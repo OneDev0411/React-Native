d140e9fee483d6d4a06fd91620a30eac859ca3cc
OneDev0411 <cryptoapple1109@gmail.com>
2023-06-22 19:45:46 +0500

    fixed token issue using mutex

--- redux/api/apiSlice.ts
+++ redux/api/apiSlice.ts
@@ -1,59 +1,80 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { baseUrl } from '../../constants';
-import { setAccessToken, setRefreshToken, logOut } from '../auth/authSlice';
-import moment from 'moment';
+import {
+  BaseQueryApi,
+  FetchArgs,
+  createApi,
+  fetchBaseQuery,
+} from "@reduxjs/toolkit/query/react";
+import { baseUrl } from "../../constants";
+import { setAccessToken, setRefreshToken, logOut } from "../auth/authSlice";
+import moment from "moment";
+import { Mutex } from "async-mutex";
+const mutex = new Mutex();
 
 const baseQuery = fetchBaseQuery({
-	baseUrl: baseUrl,
-	prepareHeaders: (headers, { getState }) => {
-		const token = getState()?.auth?.accessToken?.token;
+  baseUrl: baseUrl,
+  prepareHeaders: (headers, { getState }) => {
+    const token = getState()?.auth?.accessToken?.token;
 
-		if (token) {
-			headers.set('Authorization', `Bearer ${token}`);
-		}
-		return headers;
-	},
+    if (token) {
+      headers.set("Authorization", `Bearer ${token}`);
+    }
+    return headers;
+  },
 });
 
-const baseQueryWithReauth = async (args, api, extraOptions) => {
-	let result: any = '';
-	console.log('token expires at', api.getState()?.auth?.accessToken?.expires);
-	if (moment().isAfter(api.getState()?.auth?.accessToken?.expires)) {
-		console.log('token expired, refreshing...', api.getState()?.auth?.refreshToken?.token);
-		const refreshResult = await baseQuery(
-			{
-				url: '/auth/refresh-tokens',
-				method: 'POST',
-				body: {
-					refreshToken: `${api.getState()?.auth?.refreshToken?.token}`,
-				},
-			},
-			api,
-			extraOptions
-		);
+const baseQueryWithReauth = async (
+  args: string | FetchArgs,
+  api: BaseQueryApi,
+  extraOptions: {}
+) => {
+  console.log("expires:: ", api.getState()?.auth?.accessToken?.expires);
+  await mutex.waitForUnlock();
+  let result: any = await baseQuery(args, api, extraOptions);
 
-		if (refreshResult?.data) {
-			api.dispatch(setAccessToken(refreshResult?.data?.tokens?.access));
-			api.dispatch(setRefreshToken(refreshResult?.data?.tokens?.refresh));
-			result = await baseQuery(args, api, extraOptions);
-			console.log(
-				'refreshed token',
-				refreshResult?.data?.tokens?.access,
-				refreshResult?.data?.tokens?.refresh
-			);
-		} else if (refreshResult?.error) {
-			api.dispatch(logOut());
-			api.dispatch(apiSlice.util.resetApiState());
-			console.log('refresh token error', refreshResult?.error);
-		}
-	} else {
-		result = await baseQuery(args, api, extraOptions);
-	}
-	return result;
+  if (moment().isAfter(api.getState()?.auth?.accessToken?.expires)) {
+    if (!mutex.isLocked()) {
+      const release = await mutex.acquire();
+      try {
+        const refreshResult = await baseQuery(
+          {
+            url: "/auth/refresh-tokens",
+            method: "POST",
+            body: {
+              refreshToken: `${api.getState()?.auth?.refreshToken?.token}`,
+            },
+          },
+          api,
+          extraOptions
+        );
+
+        if (refreshResult?.data) {
+          api.dispatch(setAccessToken(refreshResult?.data?.tokens?.access));
+          api.dispatch(setRefreshToken(refreshResult?.data?.tokens?.refresh));
+          result = await baseQuery(args, api, extraOptions);
+          console.log(
+            "new refreshed token",
+            refreshResult?.data?.tokens?.access,
+            refreshResult?.data?.tokens?.refresh
+          );
+        } else if (refreshResult?.error) {
+          api.dispatch(logOut());
+          api.dispatch(apiSlice.util.resetApiState());
+          console.log("refresh token error", refreshResult?.error);
+        }
+      } finally {
+        release();
+      }
+    } else {
+      await mutex.waitForUnlock();
+      result = await baseQuery(args, api, extraOptions);
+    }
+  }
+
+  return result;
 };
 
 export const apiSlice = createApi({
-	baseQuery: baseQueryWithReauth,
-	tagTypes: ['Payouts'],
-	endpoints: (builder) => ({}),
+  baseQuery: baseQueryWithReauth,
+  tagTypes: ["Payouts"],
+  endpoints: () => ({}),
 });
